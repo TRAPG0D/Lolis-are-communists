d73d12a071e9a3b6f9c74c569ec5258d478eb45d
TRAPG0D <43757768+TRAPG0D@users.noreply.github.com>
2019-05-20 09:10:03 -0700

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,15 @@
+$("#USSR").click(function(){
+    $("#AK47").attr("src","https://www.samgerrans.com/wp-content/uploads/2013/03/communism-300x188.png");
+});
+$("#capitalist").click(function(){
+    $("#AK47").attr("src","https://pics.me.me/die-capitalist-scum-comrade-link-knows-what-to-do-28274938.png");
+});
+$("#bigBOOm").click(function(){
+    $("#bigBOOm").slideDown();
+});
+$("#Katyusha").hover(function(){
+    $("#Katyusha").fadeOut();
+});
+$("#Katyusha").click(function(){
+    $("Katyusha").fadeIn();
+});
